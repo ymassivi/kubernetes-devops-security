e4292e7eec2508e26ad02deb6cbf3a7fdaef0ada
ymassivi <ymassivi@hawk.iit.edu>
2023-04-23 18:38:35 -0500

    add jenkins signature

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -69,6 +69,14 @@ mvn -v
 
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y fontconfig openjdk-11-jre
+sudo apt-get install -y jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
